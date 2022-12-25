3e0c735ed07b8cb79212dbcff1354ca078a3f711
GMoreira05 <henrique_gabriel11@outlook.com>
2022-12-25 00:53:06 -0300

    Initial Commit

---
+++ marcadortruco/marcadortrucoApp.swift
@@ -0,0 +1,17 @@
+//
+//  marcadortrucoApp.swift
+//  marcadortruco
+//
+//  Created by Gabriel on 25/12/22.
+//
+
+import SwiftUI
+
+@main
+struct marcadortrucoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
